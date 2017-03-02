6b91654746737adb56f59f31a9241a73b39e30e2
Julian Chow <Julian Chow>
2017-03-01 19:52:26 -0500

    initial commit

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,36 @@
+# Load posh-git module from current directory
+Import-Module posh-git;
+
+$computerName = $env:COMPUTERNAME.Substring(0,1).ToUpper() + 
+                $env:COMPUTERNAME.Substring(1).ToLower();
+
+# Set up a simple prompt, adding the git prompt parts inside git repos
+function global:prompt {
+    $realLASTEXITCODE = $LASTEXITCODE
+
+    Write-Host($env:USERNAME) -NoNewline -ForegroundColor Cyan
+    Write-Host("@") -NoNewline -ForegroundColor Cyan
+    Write-Host($computerName)-NoNewline -ForegroundColor Cyan
+
+    Write-VcsStatus
+
+    $global:LASTEXITCODE = $realLASTEXITCODE
+    Write-Host " >" -ForegroundColor yellow -NoNewline
+    return " ";
+}
+
+# Aliases
+Set-Alias -name ls -value "ls.exe" -Option allscope;
+Set-Alias -name echo -value "echo.exe" -Option allscope;
+Set-Alias -name open -value "ii";
+
+function track { & git push --set-upstream $args }
+New-Alias -Name s -Value track -Force -Option AllScope;
+
+function projects { & Set-Location C:\Projects; Get-Location; }
+New-Alias -Name proj -Value projects -Force -Option AllScope;
+
+function Restart-Explorer { Stop-Process -ProcessName explorer }
+Set-alias -name restart -value Restart-Explorer;
+
+Clear-Host;
